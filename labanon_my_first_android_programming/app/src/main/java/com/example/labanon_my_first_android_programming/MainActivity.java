6d8d68b43ef5ecdc8dd61e8ca9e33bacc7376e65
John Paul Labanon <labanonpaul@sac.edu.ph>
2025-01-25 10:22:06 +0800

    added "my first activity"

---
+++ labanon_my_first_android_programming/app/src/main/java/com/example/labanon_my_first_android_programming/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.labanon_my_first_android_programming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
